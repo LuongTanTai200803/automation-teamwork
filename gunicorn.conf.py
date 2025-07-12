1b7679e795946c48cc7b420f021a354b9d19b5aa
Tai Dev <tantailg2003@gmail.com>
2025-07-12 23:18:32 +0700

    update: gunicorn, nginx in docker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 
 
-bind = "127.0.0.1:8000"
+bind = "0.0.0.0:8000"
 workers = 2
 threads = 2
 timeout = 120
--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,10 @@ from monolith_app.cronjob import schedule_daily_report
 
 app = create_app()
 schedule_daily_report()
+
+@app.route('/')
+def home():
+    return "Hello, Flask with Docker!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
